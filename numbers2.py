d3f04b438ce79125221f39c6c003a51f1eecf8b9
chrisandrestranden <chris.andre.stranden@gmail.com>
2025-07-11 00:21:32 +0200

    Add files via upload

---
+++ numbers2.py
@@ -0,0 +1,17 @@
+
+while True:
+
+    number = int(input("Enter a number (0 to quit): "))
+
+    if number == 0:
+        print("\nGoodbye!\n")
+        break
+
+    elif number > 5:
+        print("\nYour number is bigger than 5.\n")    
+    
+    elif number == 5:
+        print("\nYour number is 5.\n")
+    
+    else:
+        print("\nYour number is smaller than 5.\n")
